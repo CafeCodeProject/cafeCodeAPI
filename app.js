f2c2915590538df8dd93649b981ec4715101d24c
Deniz Erdem <deniz.erdem@renault.com>
2023-04-16 22:38:38 +0300

    mongoose and env

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-let express = require('express');
+const express = require('express');
+const mongooe = require('mongoose')
 let app = express();
 app.use(express.json());
+
+mongoose.connect(process.env.DB_STRING).then(() => {
+    console.log("db connected");
+}).catch(err => { console.log(err); });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
